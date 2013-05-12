57fbd34184aa98de09b2bc8929167561799cf9a3
Damian Skrodzki <damienix1@gmail.com>
2013-05-12 21:38:53 +0200

    Splitted

--- infiltrator.py
+++ infiltrator.py
@@ -3,15 +3,24 @@ import cv2
 
 class Infiltrator:
     def process(self, path):
-        print path
-        img = cv2.imread(path)
 
+        img = self.__load_image(path)
+        bar = self.__find_bar(img)
+        self.__show_image(bar, img)
+
+        cv2.waitKey(0)
+
+    def __load_image(self, path):
+        print "Opening " + path
+        img = cv2.imread(path)
         if img is None:
             raise Exception("There is no image under: " + path)
+        return img
+
+    def __find_bar(self, img):
 
         imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         canny = cv2.Canny(imgray, 100, 100);
-
         contours, hierarchy = cv2.findContours(canny, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         max_area = 0
 
@@ -29,15 +38,13 @@ class Infiltrator:
                 if (area > max_area):
                     max_area = area
                     bar = contour
+        return bar
 
+    def __show_image(self, bar, img):
         bound_rect = cv2.boundingRect(bar)
         pt1 = (bound_rect[0], bound_rect[1])
         pt2 = (bound_rect[0] + bound_rect[2], bound_rect[1] + bound_rect[3])
-
         cv2.rectangle(img, pt1, pt2, (255, 255, 0), 2)
-
         #Pomniejszenie obrazka
         img = cv2.resize(img, (800, 600))
-
         cv2.imshow('image', img)
-        cv2.waitKey(0)
