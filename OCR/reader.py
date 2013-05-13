db79c96ce19e4c9e4b8c592bf8490784917724fb
hauron <hauron@gmail.com>
2013-05-13 19:00:32 +0200

    comments and a bit all-around style fix

--- OCR/reader.py
+++ OCR/reader.py
@@ -7,27 +7,27 @@ class Reader:
         for img in cut_imgs:
             readStr = self.__OCR_image(img)
             readStr = readStr.replace(" ", "")
-            print "Read plate:", readStr
+            print 'Read plate:', readStr
             
     def __OCR_image(self, img):
         if _platform == "win32":
+            # First write the image file
             params = list()
             params.append(cv2.cv.CV_IMWRITE_PNG_COMPRESSION)
             params.append(8)
             cv2.imwrite("OCR/OCR_candidate.png", img, params)
             
-            args = ('OCR/tesseract.exe', 'OCR/OCR_candidate.png', 'OCR/out', 'nobatch', 'letters')
-                    
+            # Secondly run tesseract.exe and process the image
+            args = ('OCR/tesseract.exe', 'OCR/OCR_candidate.png', 'OCR/out', 'nobatch', 'letters')   
             popen = subprocess.Popen(args, stdout=subprocess.PIPE)
             popen.wait()
             output = popen.stdout.read()
             
+            # Lastly open and read the prepared recognized string
             f = open('OCR/out.txt')
             finalString = f.readline()
             f.close()
-            
             return finalString
-            
         elif _platform == "linux" or _platform == "linux2":
             print "TODO"
-            
+        return 'Linux rox!'
