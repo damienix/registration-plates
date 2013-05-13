33701cd032281fda1ed42ff957423f57e41df802
hauron <hauron@gmail.com>
2013-05-13 12:31:36 +0200

    basic OCR functionality

--- infiltrator/infiltrator.py
+++ infiltrator/infiltrator.py
@@ -1,16 +1,18 @@
 import cv2
 import qualifications
-
+import subprocess
+from sys import platform as _platform
+    
 class Infiltrator:
     quali = qualifications.Qualifications()
 	
     def process(self, path):
-
         img = self.__load_image(path)
         bars = self.__find_bars(img)
 
         for bar in bars:
             self.__draw_bar(img, bar)
+            
 
         self.__show_image(img)
         cv2.waitKey(0)
@@ -23,13 +25,11 @@ class Infiltrator:
         return img
 
     def __find_bars(self, img):
-
         bars = []
         imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         canny = cv2.Canny(imgray, 100, 100)
         contours, hierarchy = cv2.findContours(canny, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-        
         for contour in contours:
             # area = cv2.contourArea(contour)
             x, y, w, h = cv2.boundingRect(contour)
@@ -55,7 +55,8 @@ class Infiltrator:
             # Find black on white ;)
             if not self.quali.is_histogram_valid(cut_img):
                  continue
-
+            
+            self.__OCR_image(cut_img)
             bars.append(contour)
 
         return bars
@@ -71,3 +72,26 @@ class Infiltrator:
         #Pomniejszenie obrazka
         img = cv2.resize(img, (800, 600))
         cv2.imshow('image', img)
+
+    def __OCR_image(self, img):
+        if _platform == "win32":
+            params = list()
+            params.append(cv2.cv.CV_IMWRITE_PNG_COMPRESSION)
+            params.append(8)
+            cv2.imwrite("OCR_candidate.png", img, params)
+            
+            args = ("OCR/tesseract.exe", 'OCR_candidate.png', "out")
+                    
+            #Or just:
+            #args = "bin/bar -c somefile.xml -d text.txt -r aString -f anotherString".split()
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            output = popen.stdout.read()
+            
+            f = open('out.txt')
+            finalString = f.readline()
+            f.close()
+            print "Read plate:", finalString
+        elif _platform == "linux" or _platform == "linux2":
+            print "TODO"
+            
