354c843464cd56ef75c0dfab52734e507396e557
Netaro <netaro@Arenas.local>
2013-05-13 22:46:25 +0200

    Drobne poprawki do OCR'a

--- OCR/reader.py
+++ OCR/reader.py
@@ -1,6 +1,7 @@
 import cv2
 import subprocess
 from sys import platform as _platform
+from PIL import Image
 
 class Reader:
     def readText(self, cut_imgs):
@@ -17,6 +18,10 @@ class Reader:
             params.append(8)
             cv2.imwrite("OCR/OCR_candidate.png", img, params)
             
+            
+            #test -> clear image
+            self.__boldenImage("OCR/OCR_candidate.png","png")
+            
             # Secondly run tesseract.exe and process the image
             args = ('OCR/tesseract.exe', 'OCR/OCR_candidate.png', 'OCR/out', 'nobatch', 'letters')   
             popen = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -31,3 +36,25 @@ class Reader:
         elif _platform == "linux" or _platform == "linux2":
             print "TODO"
         return 'Linux rox!'
+
+    def __boldenImage(self,imagename,imageext):
+        img = Image.open(imagename)
+        img = img.convert("RGBA")
+        pixdata = img.load()
+
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
+                if pixdata[x, y][0] < 90:
+                    pixdata[x, y] = (0, 0, 0, 255)
+
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
+                if pixdata[x, y][1] < 136:
+                    pixdata[x, y] = (0, 0, 0, 255)
+
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
+                if pixdata[x, y][2] > 0:
+                    pixdata[x, y] = (255, 255, 255, 255)
+
+        img.save(imagename, imageext)
