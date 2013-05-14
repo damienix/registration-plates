8754b5fba56f5ee572e48b45a6fa940edc8721a7
Damian Skrodzki <damienix1@gmail.com>
2013-05-14 02:14:08 +0200

    Added reading tests (missing path handling in  reader.py)

--- tests/recognition.py
+++ tests/recognition.py
@@ -1,1 +1,30 @@
+import os
+import unittest
+import cv2
+from tesseract.reader import Reader
+
 __author__ = 'damienix'
+
+
+class BulkTest(unittest.TestCase):
+    def setUp(self):
+        self.reader = Reader()
+
+    def test_all(self):
+        PREFIX = "../img/plate/"
+
+        failed = []
+
+        for image in os.listdir(PREFIX):
+
+            img = cv2.imread(PREFIX + image)
+            number = self.reader.readText(img)
+
+            if image != number:
+                failed.append({'img': image, 'num': number})
+
+        for fail in failed:
+            print "%d : %s" % (fail['num'], fail['img'])
+        self.assertEqual(len(failed), 0, "More than one plate found in some images.")
+
+
